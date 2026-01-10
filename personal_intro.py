7465b9dbeed02bcfe36ffdc1bf516e20db161f14
Sudipto <153379945+Sudipto-git@users.noreply.github.com>
2026-01-10 23:28:33 +0530

    first commit

---
+++ personal_intro.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+print(f"Hello, my name is {name}. I am {age} years old and I love {hobby}.")
